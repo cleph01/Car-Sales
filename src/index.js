5cbe3eceb5ce8eaf2d51ac64d487a9df6fa28b95
Charlie <charles.j.montoya@gmail.com>
2019-10-24 11:02:51 -0400

    created store and put mock reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//import redux store 
+import { createStore } from "redux";
+
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+
+//create const variable for store
+const store = createStore(reducer)
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
